aa499ec5e5f67c97851af4c94c053eb6ae3c91b8
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-15 14:01:03 -0800

    task 0

--- main.c
+++ main.c
@@ -14,16 +14,16 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		return (EXIT_FAILURE);
 	}
-	
+
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 	execute_commands(file);
 
 	fclose(file);
-	
+
 	return (EXIT_SUCCESS);
 }
